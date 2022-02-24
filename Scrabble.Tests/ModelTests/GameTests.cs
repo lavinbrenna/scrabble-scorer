f64b14866bfbddbeba6402278124bf7365abb105
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-24 13:01:59 -0800

    add boilerplate code
    Co-Authored-By: Mikah Mathews <mikah@outofband.org>

---
+++ Scrabble.Tests/ModelTests/GameTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.VisualStudio.Test.Tools.UnitTesting;
+using Scrabble.Models;
+using System.Collections.Generic;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  public class GameTests
+  {
+
+  }
+}
---
+++ Scrabble/Models/Game.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace Scrabble.Models;
+
+public class Game
+{
+  
+}
